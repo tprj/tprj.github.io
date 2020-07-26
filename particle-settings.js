c128bf5590365c37fdf0592614c4a6141766fbc7
TProject <info@tprj.xyz>
2020-07-26 18:45:46 +0900

    ver.3.1

---
+++ particle-settings.js
@@ -0,0 +1,110 @@
+﻿particlesJS("particle", {
+  particles: {
+    number: {
+      value: 45,
+      density: {
+        enable: true,
+        value_area: 550,
+      },
+    },
+    color: {
+      value: "#b1b1b1",
+    },
+    shape: {
+      type: "circle",
+      stroke: {
+        width: 1,
+        color: "#000000",
+      },
+      polygon: {
+        nb_sides: 3,
+      },
+      image: {
+        src: "",
+        width: 100,
+        height: 100,
+      },
+    },
+    opacity: {
+      value: 0.05,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false,
+      },
+    },
+    size: {
+      value: 0,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 160,
+        size_min: 0.1,
+        sync: false,
+      },
+    },
+    line_linked: {
+      enable: true,
+      distance: 256,
+      color: "#333333",
+      opacity: 0.7,
+      width: 0.3,
+    },
+    move: {
+      enable: true,
+      speed: 5,
+      direction: "none",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200,
+      },
+    },
+  },
+  interactivity: {
+    detect_on: "window",
+    events: {
+      onhover: {
+        enable: false,
+        mode: "grab",
+      },
+      onclick: {
+        enable: true,
+        mode: "push",
+      },
+      resize: true,
+    },
+    modes: {
+      grab: {
+        distance: 400,
+        line_linked: {
+          opacity: 1,
+        },
+      },
+      bubble: {
+        distance: 400,
+        size: 40,
+        duration: 2,
+        opacity: 8,
+        speed: 3,
+      },
+      repulse: {
+        distance: 200,
+        duration: 0.4,
+      },
+      push: {
+        particles_nb: 2,
+      },
+      remove: {
+        particles_nb: 2,
+      },
+    },
+  },
+  retina_detect: true,
+});
